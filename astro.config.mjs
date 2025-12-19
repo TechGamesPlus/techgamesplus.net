c86c494351b4fe475798c0fafb9df774dae5b769
equinoxx <joshuaridley07@gmail.com>
2025-12-19 22:55:28 +1100

    Add icon support through astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://techgamesplus.net',
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
 });
