962254846863f6c5550ea63019ed075ca358d7da
Tailias <tailias.net@proton.me>
2026-07-27 19:42:43 +0800

    Tailias Updates (July 27th Commit 7)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://techgamesplus.net',
+  site: 'https://tailias.net',
   integrations: [sitemap(), icon()],
 });
